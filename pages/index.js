e3d3f4f6ac90741b6df9376a234baf9d997a0359
hendrayna55 <hendrayna55@gmail.com>
2024-08-30 06:09:37 +0700

    Tugas 9 - Hendra Ahmadillah

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function Main({ children }){
+
+  useEffect(() => {
+    fetch("/api/hello")
+    .then((res) => res.json())
+    .then((res) => console.log("response => ", res))
+    .catch((err) => console.log("error => ", err))
+  }, []);
+
   return(
     <>
       <Layout 
